68d40b9aa5b57367541b3f314825be363a0d6620
Alexandre PALO <a-palo@laposte.net>
2019-05-01 12:32:14 +0200

    cleaning

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,6 @@
+// http://a.tile.stamen.com/toner/${z}/${x}/${y}.png
+// http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+
 import React, { Component } from 'react'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet/dist/leaflet.js'
@@ -8,17 +11,9 @@ import { withPrefix } from 'gatsby'
 const getIndexForUuid = (uuid, data) => {
   return data.map(d => d.uuid).indexOf(uuid)
 }
-
-// http://a.tile.stamen.com/toner/${z}/${x}/${y}.png
-// http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-
-const zoomScale = zoom => Math.pow(zoom, 4) / 5000
-
-/*
-stopsData.forEach((d, i) => {
-  stopsData[i].push(new L.LatLng(d[2], d[3]))
-})
-*/
+const zoomScaleRadius = zoom => Math.pow(zoom, 4) / 5000
+const zoomScaleRadiusGrouped = zoom => Math.pow(zoom, 4) / 2000
+const zoomScaleText = zoom => Math.pow(zoom, 4) / 2000
 const parisCenterC = [48.8591, 2.349]
 const initialZoom = 12
 
@@ -36,29 +31,62 @@ class Map extends Component {
     const g = svg.append('g')
 
     const updateDetailedStopsElementsPositions = elements => {
-      elements.attr(
-        'transform',
-        d =>
-          'translate(' +
-          map.latLngToLayerPoint(d.latlng).x +
-          ',' +
-          map.latLngToLayerPoint(d.latlng).y +
-          ')'
-      )
-      elements.attr('r', zoomScale(map.getZoom()))
+      elements
+        .attr(
+          'transform',
+          d =>
+            'translate(' +
+            map.latLngToLayerPoint(d.latlng).x +
+            ',' +
+            map.latLngToLayerPoint(d.latlng).y +
+            ')'
+        )
+        .attr('r', zoomScaleRadius(map.getZoom()))
     }
-
-    const updateDirectLinksElementsPositions = elements => {
-      elements.attrs(d => {
-        return {
-          x1: map.latLngToLayerPoint(d[0]).x,
-          y1: map.latLngToLayerPoint(d[0]).y,
-          x2: map.latLngToLayerPoint(d[1]).x,
-          y2: map.latLngToLayerPoint(d[1]).y
+    const updateGroupedStopsTextsPositions = texts => {
+      texts
+        .attr(
+          'transform',
+          d =>
+            'translate(' +
+            map.latLngToLayerPoint(d.latlng).x +
+            ',' +
+            map.latLngToLayerPoint(d.latlng).y +
+            ')'
+        )
+        .attr('font-size', zoomScaleText(map.getZoom()))
+        .attr('dx', () => zoomScaleRadius(map.getZoom()) * 3)
+    }
+    const updateGroupedStopsElementsPositions = elements => {
+      elements
+        .attr(
+          'transform',
+          d =>
+            'translate(' +
+            map.latLngToLayerPoint(d.latlng).x +
+            ',' +
+            map.latLngToLayerPoint(d.latlng).y +
+            ')'
+        )
+        .attr('r', zoomScaleRadiusGrouped(map.getZoom()))
+    }
+    const updatelinkStopElementsPositions = elements => {
+      elements.attr('transform', d => {
+        const mid = {
+          x:
+            (map.latLngToLayerPoint(d.latlng1).x +
+              map.latLngToLayerPoint(d.latlng2).x) /
+            2,
+          y:
+            (map.latLngToLayerPoint(d.latlng1).y +
+              map.latLngToLayerPoint(d.latlng2).y) /
+            2
         }
+        return 'translate(' + mid.x + ',' + mid.y + ')'
       })
     }
 
+    // DETAILED STOPS
     d3.dsv(';', withPrefix('detailedStops.csv'), d => {
       const latitude = +d.LATITUDE.replace(',', '.')
       const longitude = +d.LONGITUDE.replace(',', '.')
@@ -68,53 +96,93 @@ class Map extends Component {
         latitude,
         longitude,
         line: d.LINE,
+        color: d.COLOR,
         latlng: new L.LatLng(latitude, longitude),
-        directLinks: d.DIRECT_LINK.split('/')
+        directLinks: d.DIRECT_LINK.split('/'),
+        changeSame: d.CHANGE_SAME.split('/'),
+        changeDiff: d.CHANGE_DIFF.split('/')
       }
     }).then(data => {
-      // Build directLinks
-      data = data.map(d => {
-        return {
-          ...d,
-          directLinksObj: d.directLinks.map(
-            dL => data[getIndexForUuid(dL, data)]
-          )
-        }
-      })
-
-      const directLinksCouples = []
-      data.forEach((d, i) => {
-        d.directLinksObj.forEach(dL => {
-          directLinksCouples.push([d.latlng, dL.latlng])
-        })
-      })
-
-      // DetailedStops
       const detailedStopElements = g
-        .selectAll('circle')
+        .selectAll('circle.detailedStop')
         .data(data)
         .enter()
         .append('circle')
-        .style('stroke', 'black')
-        .style('opacity', 0.6)
-        .style('fill', 'red')
-        .attr('r', zoomScale(initialZoom))
-
-      // DirectLink
-      const directLinksElements = g
-        .selectAll('line')
-        .data(directLinksCouples)
-        .enter()
-        .append('line')
-        .style('stroke', 'blue')
-        .style('stroke-width', 2)
+        .style('stroke', d => d.color)
+        .style('fill', d => d.color)
+        .attr('r', zoomScaleRadius(initialZoom))
 
       map.on('load movestart zoom viewreset', () => {
         updateDetailedStopsElementsPositions(detailedStopElements)
-        updateDirectLinksElementsPositions(directLinksElements)
       })
       updateDetailedStopsElementsPositions(detailedStopElements)
-      updateDirectLinksElementsPositions(directLinksElements)
+    })
+
+    // GROUPED STOPS
+    d3.dsv(';', withPrefix('groupedStops.csv'), d => {
+      const latitude = +d.LATITUDE.replace(',', '.')
+      const longitude = +d.LONGITUDE.replace(',', '.')
+      return {
+        uuid: d.UUID,
+        name: d.NAME,
+        latitude,
+        longitude,
+        latlng: new L.LatLng(latitude, longitude),
+        grouped: d.GROUPED.split('/')
+      }
+    }).then(data => {
+      const groupedStopElements = g
+        .selectAll('circle.groupedStop')
+        .data(data)
+        .enter()
+        .append('circle')
+        .style('fill', 'red')
+        .attr('opacity', 0.2)
+        .attr('r', zoomScaleRadiusGrouped(initialZoom))
+      const groupedStopsTexts = g
+        .selectAll('text.groupedStopText')
+        .data(data)
+        .enter()
+        .append('text')
+        .text(d => d.name)
+        .attr('font-weight', 700)
+        .attr('fill', 'black')
+
+      map.on('load movestart zoom viewreset', () => {
+        updateGroupedStopsElementsPositions(groupedStopElements)
+        updateGroupedStopsTextsPositions(groupedStopsTexts)
+      })
+      updateGroupedStopsElementsPositions(groupedStopElements)
+      updateGroupedStopsTextsPositions(groupedStopsTexts)
+    })
+
+    // LINKS
+    d3.dsv(';', withPrefix('linkCouples.csv'), d => {
+      const latitude1 = +d.LATITUDE1.replace(',', '.')
+      const longitude1 = +d.LONGITUDE1.replace(',', '.')
+      const latitude2 = +d.LATITUDE1.replace(',', '.')
+      const longitude2 = +d.LONGITUDE1.replace(',', '.')
+      return {
+        latlng1: new L.LatLng(latitude1, longitude1),
+        latlng2: new L.LatLng(latitude2, longitude2)
+      }
+    }).then(data => {
+      const linkCouplesElements = g
+        .selectAll('line.linkCouple')
+        .data(data)
+        .enter()
+        .append('line')
+        .style('stroke', d => 'red')
+        .style('fill', d => 'red')
+        .attr('x1', d => map.latLngToLayerPoint(d.latlng1).x)
+        .attr('x2', d => map.latLngToLayerPoint(d.latlng2).x)
+        .attr('y1', d => map.latLngToLayerPoint(d.latlng1).y)
+        .attr('y2', d => map.latLngToLayerPoint(d.latlng2).y)
+
+      map.on('load movestart zoom viewreset', () => {
+        updatelinkStopElementsPositions(linkCouplesElements)
+      })
+      updatelinkStopElementsPositions(linkCouplesElements)
     })
   }
 
